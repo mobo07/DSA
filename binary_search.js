85601f909c8b937d41ff11616706ac1ced9d4d54
Mike <mikemobo900@gmail.com>
2022-08-19 11:52:54 +0100

    added binary-search logic

---
+++ binary_search.js
@@ -0,0 +1,25 @@
+//Returns the index of the search key if found in the list
+//Only works for a sorted array
+
+function binary_search(list, key) {
+  let start = 0;
+  let end = list.length;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    if (list[mid] === key) return mid;
+    else if (list[mid] < key) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+}
+
+let arr = [];
+
+for (let i = 1; i <= 10000; ++i) {
+  arr.push(i);
+}
+
+console.log(binary_search(arr, 9995));
